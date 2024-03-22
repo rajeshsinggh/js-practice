633e934f4762c486326954dcbd4ae6f3ae620826
rajeshsinggh <159253944+rajeshsinggh@users.noreply.github.com>
2024-03-22 09:24:28 +0000

    Data types and Conversion of data types

---
+++ 01-basic/02_dataTypes.js
@@ -0,0 +1,27 @@
+"Use Strict"; // trate all JS code as newer version
+
+// alert(3 + 3) //We are using nodejs, not browser
+
+console.log(3 + 3)
+
+ console.log(3
+ + 3) // Code readability should be the high priority 
+
+console.log("Rajesh Singh")
+
+let fullName = "Ajay Singh"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// String => ""
+// boolean => true/false
+// null => standalone value or empty vlaue   // null is an object
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined 
---
+++ 01-basic/03_conversionOperation.js
@@ -0,0 +1,30 @@
+
+// let score = "Rajesh"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number (score)
+
+// console.log(typeof valueInNumber );
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; False => 0
+
+// let isLoggedIn = "Rajesh"
+
+// let booleanisLoogedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoogedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Rajesh" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber); // it will say string
