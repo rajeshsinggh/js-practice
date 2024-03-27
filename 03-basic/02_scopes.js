d7e72b7d58a3827cc04fc2bd85358c3aeb3fd84a
rajeshsinggh <159253944+rajeshsinggh@users.noreply.github.com>
2024-03-27 09:24:00 +0000

    SCopes in JS

---
+++ 03-basic/02_scopes.js
@@ -0,0 +1,13 @@
+
+// let num1 = 20
+// const num2 = 30
+// var num3 = 40
+ let num2 = 300
+if (true) {
+    let num1 = 20
+    const num2 = 30
+   console.log("inner :", num2);
+}
+// console.log(num1);
+console.log(num2);
+// console.log(num3);
